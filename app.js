edb11d34418defc7c06a2b0ec9b1677d8091f11b
jahanzaib <shahzaibbalch560@gmail.com>
2023-09-29 21:35:32 +0500

    added more code

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-alert("jahanzaib")
-alert("Shahzaib")
-alert("Aurangzaib")
-alert("Muzzammil")
-alert("Muhammad \n Saeed \n Baloch")
+var name1 = "jahanzaib"
+var name2 = "Shahzaib"
+var name3 = "Aurangzaib"
+var name4 = "Muzzammil"
+var name5 = "Muhammad \n Saeed \n Baloch"
+
+
+
+alert(name1)
+alert(name2)
+alert(name3)
+alert(name4)
+alert(name5)
